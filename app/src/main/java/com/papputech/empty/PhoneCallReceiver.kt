d450e81f275c46e099c1f77402ea310c05545017
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2023-09-21 17:52:04 +0530

    Update PhoneCallReceiver.kt

--- app/src/main/java/com/papputech/empty/PhoneCallReceiver.kt
+++ app/src/main/java/com/papputech/empty/PhoneCallReceiver.kt
@@ -9,29 +9,38 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
-
 class PhoneCallReceiver : BroadcastReceiver() {
+
+    private var wasRinging = false
+
     override fun onReceive(context: Context, intent: Intent) {
         val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
-        if (TelephonyManager.EXTRA_STATE_RINGING == state) {
-            CoroutineScope(Dispatchers.IO).launch {
-                val retrofit = Retrofit.Builder()
-                    .baseUrl("http://192.168.1.10:5050/")
-                    .build()
 
-                val api = retrofit.create(AndroidTV::class.java)
-                val response = api.pnpTV()
-                if (response.isSuccessful) {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        Toast.makeText(context, " TV API call successful", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                else {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        Toast.makeText(context, " TV API call not successful", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
+        if (TelephonyManager.EXTRA_STATE_RINGING == state) {
+            wasRinging = true
+            makeApiCall(context, "API call on ringing successful")
+        } else if (TelephonyManager.EXTRA_STATE_IDLE == state && wasRinging ) {
+            wasRinging = false
+            makeApiCall(context, "API call on call end successful")
         }
     }
+
+    private fun makeApiCall(context: Context, successMessage: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://192.168.1.10:5050/")
+                .build()
+
+            val api = retrofit.create(AndroidTV::class.java)
+            val response = api.pnpTV()
+            if (response.isSuccessful) {
+                CoroutineScope(Dispatchers.Main).launch {
+                    Toast.makeText(context, successMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+
 }
